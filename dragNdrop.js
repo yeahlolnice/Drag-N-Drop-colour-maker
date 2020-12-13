775b95c68045f0b250fb1aa8a6fec41d57ccff4d
Matthew Williams <mattwilly98@hotmail.com>
2020-12-13 22:33:59 +1100

    secondary colors are working

--- dragNdrop.js
+++ dragNdrop.js
@@ -4,7 +4,7 @@ let yellow = document.querySelector("#yellow");
 
 let first = document.querySelector("#first")
 let second = document.querySelector("#second")
-let result = document.querySelector("#result")
+let result = document.querySelector("#result-color")
 
 let colors = document.querySelectorAll(".color")
 
@@ -12,7 +12,6 @@ let colors = document.querySelectorAll(".color")
 for (var color of colors ) {
     color.addEventListener("dragstart", function(event) {
         color = event.target;
-        console.log(color)
     }, false);
 }
 
@@ -26,7 +25,30 @@ for (let div of colorDivs) {
 
     div.addEventListener('drop', function(event) {
         if (div.id != "result"){
-            event.target.appendChild(color)
+            if (div.children.length == 1) {
+                console.log(div)
+                console.log(event.originalTarget)
+                div.appendChild(color)
+                div.previousElementSibling.appendChild(event.originalTarget)
+            }else{
+                event.target.appendChild(color)
+                if (first.children.length == 1 && second.children.length == 1){
+                    let firstColor = first.children[0].id
+                    let secondColor = second.children[0].id
+                    if (firstColor == "red" || secondColor == "red") {
+                        if (firstColor == "blue" || secondColor == "blue") {
+                            result.style = "background-color: purple;"
+                        }else if (firstColor == "yellow" || secondColor == "yellow") {
+                            result.style = "background-color: orange;"
+                        }
+                    }
+                    if (firstColor == "blue" || secondColor == "blue") {
+                        if (firstColor == "yellow" || secondColor == "yellow") {
+                            result.style = "background-color: green;"
+                        }
+                    }
+                }
+            }
         }
     })
 }
